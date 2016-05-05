f8e5db94f0e3a76ad2d674452a4be6528a59ed27
lizhiguo <lizhiguoemail@163.com>
2016-05-05 17:05:20 +0800

    时钟AnalogClock 和TextClock 定时器Chronometer

---
+++ app/src/main/java/com/example/lenw/myapplication/TextClockActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenw.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextClockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_clock);
+    }
+}
