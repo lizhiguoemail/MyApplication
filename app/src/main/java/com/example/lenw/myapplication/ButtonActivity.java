e3138956e9429c05fd5ec7685a270cc6570e6c06
lizhiguo <lizhiguoemail@163.com>
2016-05-05 16:47:38 +0800

    用ToggleButton  Switch 控制LinearLayout的布局方向

--- app/src/main/java/com/example/lenw/myapplication/ButtonActivity.java
+++ app/src/main/java/com/example/lenw/myapplication/ButtonActivity.java
@@ -2,12 +2,36 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-public class ButtonActivity extends AppCompatActivity {
-
+ public class ButtonActivity extends AppCompatActivity {
+    private ToggleButton toggle;
+     private Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
+        toggle= (ToggleButton) findViewById(R.id.tb);
+        sw= (Switch) findViewById(R.id.swtest);
+       final LinearLayout lltest= (LinearLayout) findViewById(R.id.lltest);
+        CompoundButton.OnCheckedChangeListener listener=new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    lltest.setOrientation(1);
+                    toggle.setChecked(true);
+                    sw.setChecked(true);
+                }else{
+                    lltest.setOrientation(0);
+                    toggle.setChecked(false);
+                    sw.setChecked(false);
+                }
+            }
+        };
+        toggle.setOnCheckedChangeListener(listener);
+        sw.setOnCheckedChangeListener(listener);
     }
 }
