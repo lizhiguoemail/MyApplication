e6fe3685c6cd025ccb06fcfc03e181b61594a1a1
lizhiguo <lizhiguoemail@163.com>
2016-05-06 17:18:00 +0800

    时钟AnalogClock 和TextClock 定时器Chronometer

--- app/src/main/java/com/example/lenw/myapplication/TextClockActivity.java
+++ app/src/main/java/com/example/lenw/myapplication/TextClockActivity.java
@@ -1,13 +1,43 @@
 package com.example.lenw.myapplication;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class TextClockActivity extends AppCompatActivity {
-
+    Chronometer ch;
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_clock);
+        // 获取计时器组件
+        ch = (Chronometer) findViewById(R.id.test);
+        // 获取“开始”按钮
+        start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View source) {
+                // 设置开始计时时间
+                ch.setBase(SystemClock.elapsedRealtime());
+                // 启动计时器
+                ch.start();
+                start.setEnabled(false);
+            }
+        });
+        // 为Chronometer绑定事件监听器
+        ch.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer ch) {
+                // 如果从开始计时到现在超过了20s
+                if (SystemClock.elapsedRealtime() - ch.getBase() > 20 * 1000) {
+                    ch.stop();
+                    start.setEnabled(true);
+                }
+            }
+        });
     }
 }
