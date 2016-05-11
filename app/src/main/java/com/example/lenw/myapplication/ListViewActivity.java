ca20f80f87eeebce42283afe54824c0bccb05114
lizhiguo <lizhiguoemail@163.com>
2016-05-11 16:55:59 +0800

    ListView使用资源文件填充 无法控制字号大小、颜色

---
+++ app/src/main/java/com/example/lenw/myapplication/ListViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenw.myapplication;
+
+import android.os.Bundle;
+
+public class ListViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+    }
+}
