2653735419ca3f15d2ba638c32885e392e5186d1
lizhiguo <lizhiguoemail@163.com>
2016-06-20 14:54:53 +0800

    升级win10后github无法提交

--- app/src/main/java/com/example/lenw/myapplication/Test.java
+++ app/src/main/java/com/example/lenw/myapplication/Test.java
@@ -6,5 +6,6 @@ package com.example.lenw.myapplication;
 public class Test {
     public static void main(String[] args){
         System.out.println(1231231);
+        System.out.println("升级win10后github无法提交？");
     }
 }
